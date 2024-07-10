22d9ad6bda1c79660aa0ecb4019c1be0b3d3c685
AmanVatsSharma <aman95026@gmail.com>
2024-07-10 12:11:31 +0530

    remove-export-home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ type Props = {
   socials: Social[];
   projects: Project[];
 };
-export const Home: NextPage<Props>  = ( { pageInfo, experiences, skills, socials, projects} : Props ) => {
+ const Home: NextPage<Props>  = ( { pageInfo, experiences, skills, socials, projects} : Props ) => {
   console.log("Fetched socials:", socials);
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">

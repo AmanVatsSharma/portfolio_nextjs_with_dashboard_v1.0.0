76bc3e603a93bc3a24cfe65c32623eb0ead2e169
Aman Sharma <aman95026@gmail.com>
2024-07-28 09:01:24 +0530

    adding cors header test to pageInfo

--- src/pages/api/getPageInfo.ts
+++ src/pages/api/getPageInfo.ts
@@ -16,6 +16,21 @@ export default async function  handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
+
+  // Add CORS headers
+
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins or specify a specific origin
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Handle OPTIONS request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+
     const pageInfo: PageInfo[] = await sanityClient.fetch(query);
     res.status(200).json({ pageInfo })
 }
